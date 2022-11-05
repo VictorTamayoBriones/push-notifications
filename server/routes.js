6221d2d1aee55f76bd15caec7ba24ea4e01a30da
Víctor Manuel Tamayo Briones <victortamayo509@gmail.com>
2022-11-04 23:46:45 -0600

    servicios rest

--- server/routes.js
+++ server/routes.js
@@ -5,40 +5,85 @@ var router = express.Router();
 
 const mensajes = [
 
-  {
-    _id: 'XXX',
-    user: 'spiderman',
-    mensaje: 'Hola Mundo'
-  }
+    {
+        _id: 'XXX',
+        user: 'spiderman',
+        mensaje: 'Hola Mundo'
+    }
 
 ];
 
 
 // Get mensajes
-router.get('/', function (req, res) {
-  // res.json('Obteniendo mensajes');
-  res.json( mensajes );
+router.get('/', function(req, res) {
+    // res.json('Obteniendo mensajes');
+    res.json(mensajes);
 });
 
 
 // Post mensaje
-router.post('/', function (req, res) {
-  
-  const mensaje = {
-    mensaje: req.body.mensaje,
-    user: req.body.user
-  };
+router.post('/', function(req, res) {
 
-  mensajes.push( mensaje );
+    const mensaje = {
+        mensaje: req.body.mensaje,
+        user: req.body.user
+    };
 
-  console.log(mensajes);
+    mensajes.push(mensaje);
+
+    console.log(mensajes);
 
 
-  res.json({
-    ok: true,
-    mensaje
-  });
+    res.json({
+        ok: true,
+        mensaje
+    });
 });
+
+// Almacenar la suscripción
+router.post('/subscribe', (req, res) => {
+
+
+    const suscripcion = req.body;
+
+
+    push.addSubscription(suscripcion);
+
+
+    res.json('subscribe');
+
+});
+
+// Almacenar la suscripción
+router.get('/key', (req, res) => {
+
+    const key = push.getKey();
+
+
+    res.send(key);
+
+});
+
+
+// Envar una notificación PUSH a las personas
+// que nosotros queramos
+// ES ALGO que se controla del lado del server
+router.post('/push', (req, res) => {
+
+    const post = {
+        titulo: req.body.titulo,
+        cuerpo: req.body.cuerpo,
+        usuario: req.body.usuario
+    };
+
+
+    push.sendPush(post);
+
+    res.json(post);
+
+});
+
+
 
 
 
